66fa6eb48cc569bb2f9c238b057860790e8eb1c2
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-05-02 16:09:49 +0200

    Fixed the winning condition; Added checking if the piece can move if not null, before it initiates the moves

--- src/JavaChess.java
+++ src/JavaChess.java
@@ -71,13 +71,14 @@ public class JavaChess {
 
         //Actual game run
         String notation = "";//Keep notes
+        String algebraicnotation = "";//Keep notes
         Scanner input = new Scanner(System.in);
         int playerColor = -1;
         int pieceX;
         int pieceY;
-        int nextX;
-        int nextY;
+        String nextPosition;
         printTable(chessBoard);
+        System.out.println("Game has begun! Enter your move in the format abdc, where ab is the location of your piece and dc the square you want to move at. If you want to quit enter 0, if you want to surrender 00.");
         System.out.println("First Move:");
         String playerInput = input.nextLine();
         //Checking if the entered string has the correct pattern
@@ -87,27 +88,32 @@ public class JavaChess {
             if(playerInput.matches(pattern)){
                 pieceX = playerInput.charAt(0) - '0';
                 pieceY = playerInput.charAt(1) - '0';
-                nextX = playerInput.charAt(2) - '0';
-                nextY = playerInput.charAt(3) - '0';
+                nextPosition = playerInput.substring(2,4);
                 //Checks if input out of range
-                if(((nextX<0 || nextX>7)|| (nextY<0 || nextY>7))||((pieceX<0 || pieceX>7)||(pieceY<0 || pieceY>7))){
-                    System.out.println("The square is out range! Please enter a valid move:");
-                }//Checks if sqaure is empty
-                else if (chessBoard[pieceX][pieceY] == null) {
+                //((nextX<0 || nextX>7)|| (nextY<0 || nextY>7))||((pieceX<0 || pieceX>7)||(pieceY<0 || pieceY>7))
+                if(chessBoard[pieceX][pieceY] == null){
                     System.out.println("The space is empty! Please enter another move:");
+                }//Checks if sqaure is empty
+                else if (!chessBoard[pieceX][pieceY].freeToMove(nextPosition, chessBoard)) {
+                    System.out.println("The move is illegal or the square is out range! Please enter a valid move:");
                 }//Checks if piece doesn't match color
                 else if (chessBoard[pieceX][pieceY].color != playerColor) {
                     System.out.println("Player can't move piece of opposite color! Please enter the move again:");
                 }//If none the earlier cases occur, moves the piece
                 else{
-                    notation += pieceX + "" + pieceY + chessBoard[pieceX][pieceY].getPieceClass() + playerInput.substring(2,4) + "\n";
-                    //Checks if the next square has the King
-                    if((chessBoard[pieceX][pieceY].pieceClass).equals("King")){
-                        String winningSentence = (playerColor==-1)?"White " + " has won!":"Black " + " has won!";
-                        System.out.println(winningSentence);
-                        break;
+                    notation += pieceX + "" + pieceY + chessBoard[pieceX][pieceY].getPieceClass() + nextPosition + "\n";
+                    algebraicnotation += chessBoard[pieceX][pieceY].getPieceClass() + nextPosition + "\n";
+                    //Checks if the next square is empty, and if not if it has the King
+                    if(chessBoard[nextPosition.charAt(0)-'0'][nextPosition.charAt(1)-'0']!=null){
+                        if((chessBoard[nextPosition.charAt(0)-'0'][nextPosition.charAt(1)-'0'].getPieceClass())=='K'){
+                            String winningSentence = (playerColor==-1)?"White " + " has won!":"Black " + " has won!";
+                            System.out.println(winningSentence);
+                            break;
+
+                    }
+                    
                     }//Does the actual moving, prints "Next move:" in the terminal, and changes player color
-                    chessBoard[pieceX][pieceY].initiateMove(playerInput.substring(2,4),chessBoard);
+                    chessBoard[pieceX][pieceY].initiateMove(nextPosition,chessBoard);
                     System.out.println("Next move:");
                     playerColor *=(-1);
                 }
@@ -130,9 +136,14 @@ public class JavaChess {
             String surrenderSentence = (playerColor==-1)?"White " + " has surrendered. Black":"Black " + " has surrendered! White";
             System.out.println(surrenderSentence+" has won!");
 
+        }//Checks if player exited the game
+        else if(playerInput.equals("0")){
+            System.out.println("Game quit.");
+
         }
         //Prints the games complete notation and closes scanner
         System.out.println(notation);
+        System.out.println("Algebraic notation:\n" + algebraicnotation);
         input.close();
 
 
