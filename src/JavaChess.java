3300ffae6a932fb115447e99d43f50801f8af5db
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-05-02 17:55:30 +0200

    Just in case

--- src/JavaChess.java
+++ src/JavaChess.java
@@ -12,7 +12,7 @@ public class JavaChess {
         }
         System.out.println("");
         for(int i=0;i<8;i++){
-            System.out.printf("%-3d", i);
+            System.out.printf("%-3d",i);
             for(int j=0;j<8;j++){
                 
                 if(boardReference[i][j]!=null){
